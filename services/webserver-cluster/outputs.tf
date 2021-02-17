bca40826c1db08d336ff4052c1cea89e46e05f8d
bidd <biddqb@gmail.com>
2021-02-17 10:41:43 +0700

    Fix output

--- services/webserver-cluster/outputs.tf
+++ services/webserver-cluster/outputs.tf
@@ -12,8 +12,3 @@ output "asg_name" {
   value       = aws_autoscaling_group.asg_ec2.name
   description = "The name of the Auto Scaling Group"
 }
-
-output "clb_dns_name" {
-  value       = aws_elb.ec2_elb.dns_name
-  description = "The domain name of the load balancer"
-}
