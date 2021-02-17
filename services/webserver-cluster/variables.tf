19d4190cd46b4099b07530caf2223b26be3303a3
bidd <biddqb@gmail.com>
2021-02-17 10:47:25 +0700

    Fix variable

--- services/webserver-cluster/variables.tf
+++ services/webserver-cluster/variables.tf
@@ -1,20 +1,3 @@
-variable "aws_access_key" {
-  type = string
-  description = "AWS access key"
-}
-variable "aws_secret_key" {
-  type = string
-  description = "AWS secret key"
-}
-variable "aws_region" {
-  type = string
-  description = "AWS region"
-}
-variable "aws_availability_zone" {
-  type = string
-  description = "AWS Availability Zone"
-}
-
 variable "server_port" {
   type = number
   description = "Port for connecting to webserver"
